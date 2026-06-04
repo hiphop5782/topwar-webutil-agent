4f802bbb8942d4f5986e6bafa73080c6e107977a
hiphop5782 <zdlsz@hanmail.net>
2026-06-04 16:34:35 +0900

    init project

---
+++ topwar-helper-agent/src/main/java/com/hacademy/twagent/TopwarHelperAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hacademy.twagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopwarHelperAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopwarHelperAgentApplication.class, args);
+	}
+
+}
---
+++ topwar-helper-agent/src/test/java/com/hacademy/twagent/TopwarHelperAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacademy.twagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopwarHelperAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
